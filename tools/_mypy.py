6554bc983eaaa6ed6dee5e63da8893ade7110e76
zedzhen <59135268+zedzhen@users.noreply.github.com>
2026-06-21 17:02:47 +0300

    run mypy for PR

--- tools/_mypy.py
+++ tools/_mypy.py
@@ -7,4 +7,5 @@ base_dir = Path(__file__).resolve().parent.parent
 run([sys.executable, "-m", "pip", "install", "pip", "-U"])
 run([sys.executable, "-m", "pip", "install", "--group", "types"])
 for py_version in ["3.10", "3.11", "3.12", "3.13", "3.14"]:
+    print(f"python version: {py_version}")
     run([sys.executable, "-m", "mypy", "--python-version", py_version]).check_returncode()
